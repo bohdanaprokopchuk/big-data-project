59591ba0448abee7b8a442b4862e85a4f5412c82
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2024-10-08 20:59:51 +0300

    Initialized Spark session

---
+++ main.py
@@ -0,0 +1,8 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+spark_session = (SparkSession.builder
+                             .master("local")
+                             .appName("task app")
+                             .config(conf=SparkConf())
+                             .getOrCreate())
