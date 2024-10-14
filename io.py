7acb456fa6d6612425e184a48585ae0a9238769c
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2024-10-15 01:09:04 +0300

    Read DataFrame for name.basics.tsv.gz

--- io.py
+++ io.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
 from settings import FILE_PATH
 
-spark = SparkSession.builder.appName('TitleRatingsProcessing').getOrCreate()
+spark = SparkSession.builder.appName('NameBasicsProcessing').getOrCreate()
 
 
-def read_title_ratings_df(path=FILE_PATH):
+def read_name_basics_df(path=FILE_PATH):
     return spark.read.csv(path, sep='\t', header=True)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from basic_dfs.basic_df_prokopchuk import basic_test_df
-from io import read_title_ratings_df
+from io import read_name_basics_df
 
 spark_session = (SparkSession.builder
                              .master("local")
@@ -10,7 +10,7 @@ spark_session = (SparkSession.builder
                              .getOrCreate())
 
 
-df = read_title_ratings_df()
+df = read_name_basics_df()
 
 
 if __name__ == "__main__":
