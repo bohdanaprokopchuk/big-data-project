525e38fbe03fe120d0ffa59b9c44b6759b55a633
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2024-10-21 16:19:17 +0300

    Implement CSV writing for name_basics_csv DataFrame

--- io_data.py
+++ io_data.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-from settings import FILE_PATH
+from settings import FILE_PATH, OUTPUT_PATH
 
 
 name_basics_schema = StructType([
@@ -26,3 +27,7 @@ def read_name_basics_df(spark_session, path=FILE_PATH):
         escape="\\",
         dateFormat='yyyy'
     )
+
+
+def write_name_basics_df_to_csv(df: DataFrame, path=OUTPUT_PATH, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from io import read_name_basics_df
+from io_data import read_name_basics_df, write_name_basics_df_to_csv
 
 spark_session = (SparkSession.builder
                              .master("local")
@@ -11,3 +11,4 @@ spark_session = (SparkSession.builder
 
 if __name__ == "__main__":
     df = read_name_basics_df(spark_session)
+    write_name_basics_df_to_csv(df, path="data/results/name_basics_csv", mode="overwrite", num_partitions=2)
