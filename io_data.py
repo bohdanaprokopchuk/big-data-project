67cb23577beaa6ae0091aeadaaf10c887c1d38aa
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2024-10-27 02:22:23 +0300

    Export DataFrame to CSV with parameters

--- io_data.py
+++ io_data.py
@@ -1,7 +1,12 @@
-from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-from settings import FILE_PATH, OUTPUT_PATH
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+from settings import (FILE_PATH_NAME_BASICS, FILE_PATH_TITLE_AKAS,
+                      FILE_PATH_TITLE_BASICS, FILE_PATH_TITLE_CREW,
+                      FILE_PATH_TITLE_EPISODE, FILE_PATH_TITLE_PRINCIPALS,
+                      FILE_PATH_TITLE_RATINGS, OUTPUT_PATH_NAME_BASICS,
+                      OUTPUT_PATH_TITLE_AKAS, OUTPUT_PATH_TITLE_BASICS,
+                      OUTPUT_PATH_TITLE_CREW, OUTPUT_PATH_TITLE_EPISODE,
+                      OUTPUT_PATH_TITLE_PRINCIPALS, OUTPUT_PATH_TITLE_RATINGS)
 
 
 name_basics_schema = StructType([
@@ -14,7 +19,7 @@ name_basics_schema = StructType([
 ])
 
 
-def read_name_basics_df(spark_session, path=FILE_PATH):
+def read_name_basics_df(spark_session, path=FILE_PATH_NAME_BASICS):
     return spark_session.read.csv(
         path,
         sep='\t',
@@ -29,5 +34,145 @@ def read_name_basics_df(spark_session, path=FILE_PATH):
     )
 
 
-def write_name_basics_df_to_csv(df: DataFrame, path=OUTPUT_PATH, mode="overwrite", num_partitions=1):
+def write_name_basics_df_to_csv(df: DataFrame, path=OUTPUT_PATH_NAME_BASICS, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
+
+
+title_akas_schema = StructType([
+    StructField("titleId", StringType(), True),
+    StructField("ordering", IntegerType(), True),
+    StructField("title", StringType(), True),
+    StructField("region", StringType(), True),
+    StructField("language", StringType(), True),
+    StructField("types", StringType(), True),
+    StructField("attributes", StringType(), True),
+    StructField("isOriginalTitle", IntegerType(), True)
+])
+
+def read_title_akas_df(spark_session, path=FILE_PATH_TITLE_AKAS):
+    return spark_session.read.csv(
+        path,
+        sep='\t',
+        header=True,
+        schema=title_akas_schema,
+        inferSchema=False,
+        mode='DROPMALFORMED',
+        nullValue='\\N'
+    )
+
+def write_title_akas_df_to_csv(df: DataFrame, path=OUTPUT_PATH_TITLE_AKAS, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
+
+title_basics_schema = StructType([
+    StructField("tconst", StringType(), True),
+    StructField("titleType", StringType(), True),
+    StructField("primaryTitle", StringType(), True),
+    StructField("originalTitle", StringType(), True),
+    StructField("isAdult", IntegerType(), True),
+    StructField("startYear", IntegerType(), True),
+    StructField("endYear", IntegerType(), True),
+    StructField("runtimeMinutes", IntegerType(), True),
+    StructField("genres", StringType(), True)
+])
+
+def read_title_basics_df(spark_session, path=FILE_PATH_TITLE_BASICS):
+    return spark_session.read.csv(
+        path,
+        sep='\t',
+        header=True,
+        schema=title_basics_schema,
+        inferSchema=False,
+        mode='DROPMALFORMED',
+        nullValue='\\N'
+    )
+
+def write_title_basics_df_to_csv(df: DataFrame, path=OUTPUT_PATH_TITLE_BASICS, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
+
+
+title_crew_schema = StructType([
+    StructField("tconst", StringType(), True),
+    StructField("directors", StringType(), True),
+    StructField("writers", StringType(), True)
+])
+
+def read_title_crew_df(spark_session, path=FILE_PATH_TITLE_CREW):
+    return spark_session.read.csv(
+        path,
+        sep='\t',
+        header=True,
+        schema=title_crew_schema,
+        inferSchema=False,
+        mode='DROPMALFORMED',
+        nullValue='\\N'
+    )
+
+def write_title_crew_df_to_csv(df: DataFrame, path=OUTPUT_PATH_TITLE_CREW, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
+
+
+title_episode_schema = StructType([
+    StructField("tconst", StringType(), True),
+    StructField("parentTconst", StringType(), True),
+    StructField("seasonNumber", StringType(), True),
+    StructField("episodeNumber", StringType(), True)
+])
+
+def read_title_episode_df(spark_session, path=FILE_PATH_TITLE_EPISODE):
+    return spark_session.read.csv(
+        path,
+        sep='\t',
+        header=True,
+        schema=title_episode_schema,
+        inferSchema=False,
+        mode='DROPMALFORMED',
+        nullValue='\\N'
+    )
+
+def write_title_episode_df_to_csv(df: DataFrame, path=OUTPUT_PATH_TITLE_EPISODE, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
+
+
+title_principals_schema = StructType([
+    StructField("tconst", StringType(), True),
+    StructField("ordering", IntegerType(), True),
+    StructField("nconst", StringType(), True),
+    StructField("category", StringType(), True),
+    StructField("job", StringType(), True),
+    StructField("characters", StringType(), True)
+])
+
+def read_title_principals_df(spark_session, path=FILE_PATH_TITLE_PRINCIPALS):
+    return spark_session.read.csv(
+        path,
+        sep='\t',
+        header=True,
+        schema=title_principals_schema,
+        inferSchema=False,
+        mode='DROPMALFORMED',
+        nullValue='\\N'
+    )
+
+def write_title_principals_df_to_csv(df: DataFrame, path=OUTPUT_PATH_TITLE_PRINCIPALS, mode="overwrite", num_partitions=1):
+    df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
+
+
+title_ratings_schema = StructType([
+    StructField("tconst", StringType(), True),
+    StructField("averageRating", DoubleType(), True),
+    StructField("numVotes", IntegerType(), True)
+])
+
+def read_title_ratings_df(spark_session, path=FILE_PATH_TITLE_RATINGS):
+    return spark_session.read.csv(
+        path,
+        sep='\t',
+        header=True,
+        schema=title_ratings_schema,
+        inferSchema=False,
+        mode='DROPMALFORMED',
+        nullValue='\\N'
+    )
+
+def write_title_ratings_df_to_csv(df: DataFrame, path=OUTPUT_PATH_TITLE_RATINGS, mode="overwrite", num_partitions=1):
     df.repartition(num_partitions).write.csv(path, header=True, mode=mode)
--- main.py
+++ main.py
@@ -1,14 +1,55 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from io_data import read_name_basics_df, write_name_basics_df_to_csv
+from io_data import (
+    read_name_basics_df, write_name_basics_df_to_csv,
+    read_title_akas_df, write_title_akas_df_to_csv,
+    read_title_basics_df, write_title_basics_df_to_csv,
+    read_title_crew_df, write_title_crew_df_to_csv,
+    read_title_episode_df, write_title_episode_df_to_csv,
+    read_title_principals_df, write_title_principals_df_to_csv,
+    read_title_ratings_df, write_title_ratings_df_to_csv
+)
 
 spark_session = (SparkSession.builder
                              .master("local")
-                             .appName("NameBasicsProcessing")
+                             .appName("IMDB Data Processing")
                              .config(conf=SparkConf())
                              .getOrCreate())
 
-
 if __name__ == "__main__":
-    df = read_name_basics_df(spark_session)
-    write_name_basics_df_to_csv(df, path="data/results/name_basics_csv", mode="overwrite", num_partitions=2)
+    name_basics_df = read_name_basics_df(spark_session)
+    name_basics_df.printSchema()  # Вивести схему
+    name_basics_df.show(5)         # Вивести перші 5 рядків
+    write_name_basics_df_to_csv(name_basics_df, path="data/results/name_basics_csv", mode="overwrite", num_partitions=2)
+
+    title_akas_df = read_title_akas_df(spark_session)
+    title_akas_df.printSchema()     # Вивести схему
+    title_akas_df.show(5)           # Вивести перші 5 рядків
+    write_title_akas_df_to_csv(title_akas_df, path="data/results/title_akas_csv", mode="overwrite", num_partitions=2)
+
+    title_basics_df = read_title_basics_df(spark_session)
+    title_basics_df.printSchema()   # Вивести схему
+    title_basics_df.show(5)         # Вивести перші 5 рядків
+    write_title_basics_df_to_csv(title_basics_df, path="data/results/title_basics_csv", mode="overwrite", num_partitions=2)
+
+    title_crew_df = read_title_crew_df(spark_session)
+    title_crew_df.printSchema()      # Вивести схему
+    title_crew_df.show(5)            # Вивести перші 5 рядків
+    write_title_crew_df_to_csv(title_crew_df, path="data/results/title_crew_csv", mode="overwrite", num_partitions=2)
+
+    title_episode_df = read_title_episode_df(spark_session)
+    title_episode_df.printSchema()    # Вивести схему
+    title_episode_df.show(5)          # Вивести перші 5 рядків
+    write_title_episode_df_to_csv(title_episode_df, path="data/results/title_episode_csv", mode="overwrite", num_partitions=2)
+
+    title_principals_df = read_title_principals_df(spark_session)
+    title_principals_df.printSchema()  # Вивести схему
+    title_principals_df.show(5)        # Вивести перші 5 рядків
+    write_title_principals_df_to_csv(title_principals_df, path="data/results/title_principals_csv", mode="overwrite", num_partitions=2)
+
+    title_ratings_df = read_title_ratings_df(spark_session)
+    title_ratings_df.printSchema()     # Вивести схему
+    title_ratings_df.show(5)           # Вивести перші 5 рядків
+    write_title_ratings_df_to_csv(title_ratings_df, path="data/results/title_ratings_csv", mode="overwrite", num_partitions=2)
+
+spark_session.stop()
